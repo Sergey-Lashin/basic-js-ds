c50837992a96a794ffd2e5aece68c0aef591eb1f
Sergey-Lashin <heavyra1n4222@gmail.com>
2025-01-05 20:27:17 +0500

    feat: implement queue task

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    }
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue( value ) {
+    const newLastNode = new Node (value);
+    if (this.tail) {
+      this.tail.next = newLastNode;
+      } else {
+        this.head = newLastNode;
+      } this.tail = newLastNode;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return;
+    }
+    const removeValue = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return removeValue;
   }
 }
 
